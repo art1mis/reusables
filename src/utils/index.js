0647cedbed0e544286d6fbf59004786872a537d2
Art1mis <malik0786sam@gmail.com>
2022-01-20 21:32:45 +0530

    Fixed export typo

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,4 @@
 // Utilities
-export { default as isPlaintObject } from "./isPlainObject";
+export { default as isPlainObject } from "./isPlainObject";
 export { default as deepmerge } from "./deepmerge";
 export { getCookie, setCookie } from "./cookie";
