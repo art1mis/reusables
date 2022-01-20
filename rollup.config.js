8e409b22d77430002bfc4570cbe0c9c050c8c7b7
Art1mis <malik0786sam@gmail.com>
2022-01-20 22:15:03 +0530

    Test Fix 3

--- rollup.config.js
+++ rollup.config.js
@@ -1,36 +1,34 @@
 import babel from "rollup-plugin-babel";
-import { nodeResolve } from "@rollup/plugin-node-resolve";
+import resolve from "@rollup/plugin-node-resolve";
 import external from "rollup-plugin-peer-deps-external";
 import { terser } from "rollup-plugin-terser";
 import postcss from "rollup-plugin-postcss";
 import gzip from "rollup-plugin-gzip";
 
-export default [
-  {
-    input: "./src/index.js",
-    output: [
-      {
-        file: "./dist/index.js",
-        format: "cjs",
-      },
-      {
-        file: "./dist/index.es.js",
-        format: "es"
-      },
-    ],
-    plugins: [
-      postcss({
-        plugins: [],
-        minimize: true,
-      }),
-      babel({
-        exclude: "node_modules/**",
-        presets: ["@babel/preset-react"],
-      }),
-      external(),
-      nodeResolve(),
-      terser(),
-      gzip()
-    ],
-  },
-];
+export default {
+  input: "./index.js",
+  output: [
+    {
+      format: "cjs",
+      file: "./dist/index.js",
+    },
+    {
+      file: "./dist/index.mjs",
+      format: "esm",
+    },
+  ],
+  plugins: [
+    resolve(),
+    postcss({
+      plugins: [],
+      minimize: true,
+    }),
+    babel({
+      exclude: "node_modules/**",
+      presets: ["@babel/preset-react"],
+    }),
+    external(),
+    terser(),
+    gzip(),
+  ],
+};
--- src/utils/isPlainObject.js
+++ src/utils/isPlainObject.js
@@ -1,9 +1,8 @@
-const isPlainObject = (item) =>(
-    item !== null &&
-    typeof item === "object" &&
-    // TS thinks `item is possibly null` even though this was our first guard.
-    // @ts-expect-error
-    item.constructor === Object
-  );
+const isPlainObject = (item) =>
+  item !== null &&
+  typeof item === "object" &&
+  // TS thinks `item is possibly null` even though this was our first guard.
+  // @ts-expect-error
+  item.constructor === Object;
 
-export default isPlainObject
+export default isPlainObject;
